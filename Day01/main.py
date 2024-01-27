addc845b104e915ac8c5ed474b01966aef3be83d
Rich Robinson <rich.robinson84@gmail.com>
2024-01-27 21:25:04 +0000

    End of Day 1 state

---
+++ Day01/main.py
@@ -0,0 +1,14 @@
+print("Rich Robinson")
+print("Saturday 27th January 2024")
+
+print("""
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+""")
+
+# How to print emojis?
+# https://www.geeksforgeeks.org/python-program-to-print-emojis/
+# https://medium.com/analytics-vidhya/how-to-print-emojis-using-python-2e4f93443f7e
+
+print("I am feeling \N{winking face}")
