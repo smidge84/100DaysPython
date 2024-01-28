1369fe4c87b59af1490bd56f0f87519a13d43835
Rich Robinson <rich.robinson84@gmail.com>
2024-01-28 14:02:24 +0000

    Day 2 complete

---
+++ Day02/challenge.py
@@ -0,0 +1,20 @@
+# Asks for the user's name, favorite food, favorite music and where they live
+# Store all of the answers in different variables.
+# Print out a full sentence that includes the user's favorite things.
+# Give them a positive affirmation at the end - tell them they rock at doing something!
+
+print("Can I ask you some questions to get to know you?")
+
+name = input("Whats your name?: ")
+food = input("What's your favourite food?: ")
+music = input("What's your favourite genre of music?: ")
+home = input("Where for you live?: ")
+
+print("Nice to meet you")
+print(name)
+print(food)
+print("That's my favourite food too.")
+print("You have good taste in music. That's a good genre.")
+print(music)
+print(home)
+print("That's a good place to live.")
---
+++ Day02/main.py
@@ -0,0 +1,8 @@
+myName = input("What's your name?: ")
+myAge = input("How old are you?: ")
+
+print()
+print("So, you are")
+print(myName)
+print("and are the ripe old age of")
+print(myAge)
