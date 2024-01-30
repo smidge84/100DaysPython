2343bbfa809d03a1ff86fd8d193b5bfa09f10d19
Rich Robinson <rich.robinson84@gmail.com>
2024-01-30 17:26:52 +0000

    Day 7 complete

---
+++ Day07/main.py
@@ -0,0 +1,12 @@
+tvShow = input("What is your favourite tv show? ")
+if tvShow == "peppa pig":
+  print("Ugh, why?")
+  faveCharacter = input("Who is your favourite character? ")
+  if faveCharacter == "daddy pig":
+    print("Right answer")
+  else:
+    print("Nah, Daddy Pig's the greatest")
+elif tvShow == "paw patrol":
+  print("Aww, sad times")
+else:
+  print("Yeah, that's cool and all…")
